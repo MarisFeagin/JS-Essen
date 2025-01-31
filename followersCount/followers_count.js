3b7db3e7f6b010a072109f290a1fc88cccf1906f
MarisFeagin <mfeagin46@gmail.com>
2025-01-31 00:01:25 -0500

    Added followers_count folder and files

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0; // init count to 0
+
+function increaseCount() {
+  count++; //increment count by 1
+  displayCount(); //display count
+  checkCountValue(); // Check count value and display messages
+}
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count; // display count in the html
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 followers! Congrats!");
+    } else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
