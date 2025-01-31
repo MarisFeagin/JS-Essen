1f7e2f5857713cd8cd4470d3735df78d49a5ae44
MarisFeagin <mfeagin46@gmail.com>
2025-01-31 01:24:02 -0500

    added Practice Task to Product Survey Files, still JS issue

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -5,6 +5,7 @@
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experince = document.getElementById('experince').value;
 
     const submitButton = document.getElementById('submitBtn');
 
@@ -19,6 +20,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperince').innerHTML = experince;
 
     document.getElementById('userInfo').style.display = 'block';
 };
