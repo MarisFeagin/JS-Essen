c7094da5646a1f3667bd943ea033fa463764c8f0
MarisFeagin <mfeagin46@gmail.com>
2025-01-30 23:40:08 -0500

    Added Practice Task to Calculate_Area Files

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,4 +8,14 @@ function calculateArea() {
     let area = length * width;
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
-}
+};
+
+function calculateAmount() {
+     grocery1 = parseFloat(document.getElementById('grocery-1').value);
+     grocery2 = parseFloat(document.getElementById('grocery-2').value);
+     grocery3 = parseFloat(document.getElementById('grocery-3').value);
+
+     let amount = grocery1 + grocery2 + grocery3;
+
+     document.getElementById('total').innerText = `The total amount is: $ ${amount}`;
+};
