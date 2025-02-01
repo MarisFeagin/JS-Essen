4946b6465dceeccb9accaf16455226db8e532b21
MarisFeagin <mfeagin46@gmail.com>
2025-01-31 21:49:08 -0500

    Added delete button to Book System File, function bugged

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ function showbooks() {
          <p><strong>Book Name: </strong>${book.name}</p>
          <p><strong>Author Name: </strong>${book.authorName}</p>
          <p><strong>Book Description: </strong>${book.bookDescription}</p>
-         <p><strong>No. of Pages: </strong>${book.pagesNumber}</p>`
+         <p><strong>No. of Pages: </strong>${book.pagesNumber}</p>
+         <button onclick="deletebook()">Delete</button>`
 
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
@@ -36,4 +37,9 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deletebook() {
+    const books = books.splice(index, 1);
+    showbooks();
 }
